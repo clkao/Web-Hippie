076a12e0fb9098adb8a0087f3f08b35d8ec04b70
Chia-liang Kao <clkao@clkao.org>
2011-08-03 21:40:00 +0800

    Checking in changes prior to tagging of version 0.37.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index be49adf..985a859 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Web::Hippie
    
    +0.37 Wed Aug  3 21:38:54 CST 2011
    +        - use protocol::websocket, so we can support draft-ietf-hybi-10.
    +
     0.36 Sun Feb  6 21:29:11 CST 2011
             - silence a warning caused by bad use of class::accessor constructor.

--- lib/Web/Hippie.pm
+++ lib/Web/Hippie.pm
@@ -2,7 +2,7 @@ package Web::Hippie;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 use parent 'Plack::Middleware';
 
 use Plack::Util::Accessor qw( on_error on_message trusted_origin );
